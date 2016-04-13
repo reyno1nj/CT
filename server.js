01ef04e207b86f09e113b721296deeb4f8e65669
Nolan Reynolds <nolanreynolds@Nolans-MacBook-Pro.local>
2016-04-13 15:05:43 -0400

    get Trump tweets

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ var client = new Twitter({
   access_token_key: '712102656307757056-CZmHQ0w4Jwwq0dX4GfIHycXU2GDEp59',
   access_token_secret: 'uJPSKjHIlCbCU03Vl1yDqJnMWgDRkwO7iigKLJV2MYQnU'
 });
-var candidate = 'Trump'
+var candidate = 'Cruz'
 client.stream('statuses/filter', {track: candidate}, function(stream) {
   stream.on('data', function(tweet) {
     //console.log(tweet.text);
